09008c42c45757cadf9096d4cb69af791a66e63a
Olga16119 <tkachenko93@ukr.net>
2024-08-02 21:56:56 +0200

    changen index HTML and JS files

--- js/index.js
+++ js/index.js
@@ -1,21 +1,51 @@
 function search(event) {
   event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+  let apiKey = "ef0t819co4fbb2a23ffa030681b76557";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(displayTemperature);
 }
+function displayTemperature(responce) {
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = responce.data.city;
 
-let searchForm = document.querySelector("form");
+  let temperature = document.querySelector(".current-temperature-value");
+  temperature.innerHTML = Math.round(responce.data.temperature.current);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
-let searchInput = document.querySelector(".search-input");
-let city = document.querySelector(".current-city");
-city.innerHTML = `${searchInput.value}`;
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
 
-let apiKey = "ef0t819co4fbb2a23ffa030681b76557";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-function displayTemperature(responce) {
-  city = responce.data.city;
-  console.log(responce);
-  let temperature = document.querySelector(".current-temperature-value");
-  temperature = responce.data.temperature.current;
-}
+currentDateELement.innerHTML = formatDate(currentDate);
